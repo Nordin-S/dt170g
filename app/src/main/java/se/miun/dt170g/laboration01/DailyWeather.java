3f64bfd7c5848139a2be8635e1e3ed8f5457128e
hejtlo0p <peter.stegeby@gmail.com>
2023-01-20 16:01:05 +0100

    Removed setters, added units for temp, windspeed, cloudiness, and raining. Added function to refresh DailyWeather.

--- app/src/main/java/se/miun/dt170g/laboration01/DailyWeather.java
+++ app/src/main/java/se/miun/dt170g/laboration01/DailyWeather.java
@@ -1,70 +1,72 @@
 package se.miun.dt170g.laboration01;
 
 public class DailyWeather {
-
-    private double temperature;
-    private double windSpeed;
-    private String windDirection;
-    private double cloudiness;
-    private double raining_min;
-    private double raining_max;
-
+    private final double temperature;
+    private final String temperatureUnit;
+    private final double windSpeed;
+    private final String windSpeedUnit;
+    private final String windDirection;
+    private final double cloudiness;
+    private final String cloudinessUnit;
+    private final double raining_min;
+    private final double raining_max;
+    private final String rainingUnit;
 
     // Constructor
     DailyWeather(double temperature, double windSpeed, String windDirection, double cloudiness, double raining_min, double raining_max) {
-        this.temperature = temperature;
-        this.windSpeed = windSpeed;
-        this.windDirection = windDirection;
-        this.cloudiness = cloudiness;
-        this.raining_min = raining_min;
-        this.raining_max = raining_max;
+        this.temperature    = temperature;
+        this.windSpeed      = windSpeed;
+        this.windDirection  = windDirection;
+        this.cloudiness     = cloudiness;
+        this.raining_min    = raining_min;
+        this.raining_max    = raining_max;
+        this.temperatureUnit= "Celsius";
+        this.windSpeedUnit  = "mps";
+        this.cloudinessUnit = "%";
+        this.rainingUnit    = "mm";
     }
 
     public double getTemperature() {
         return temperature;
     }
 
-    public void setTemperature(double temperature) {
-        this.temperature = temperature;
-    }
-
     public double getWindSpeed() {
         return windSpeed;
-    }
-
-    public void setWindSpeed(double windSpeed) {
-        this.windSpeed = windSpeed;
     }
 
     public String getWindDirection() {
         return windDirection;
     }
 
-    public void setWindDirection(String windDirection) {
-        this.windDirection = windDirection;
-    }
-
     public double getCloudiness() {
         return cloudiness;
-    }
-
-    public void setCloudiness(double cloudiness) {
-        this.cloudiness = cloudiness;
     }
 
     public double getRaining_min() {
         return raining_min;
     }
 
-    public void setRaining_min(double raining_min) {
-        this.raining_min = raining_min;
-    }
-
     public double getRaining_max() {
         return raining_max;
     }
 
-    public void setRaining_max(double raining_max) {
-        this.raining_max = raining_max;
+    public String getTemperatureUnit() {
+        return temperatureUnit;
+    }
+
+    public String getWindSpeedUnit() {
+        return windSpeedUnit;
+    }
+
+    public String getCloudinessUnit() {
+        return cloudinessUnit;
+    }
+
+    public String getRainingUnit() {
+        return rainingUnit;
+    }
+
+    public DailyWeather refreshDailyWeather(double newTemperature, double newWindSpeed, String newWindDirection, double newCloudiness, double newRaining_min, double newRaining_max) {
+        return new DailyWeather(newTemperature, newWindSpeed, newWindDirection, newCloudiness, newRaining_min, newRaining_max);
     }
 }
