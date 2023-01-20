54c8f3764ecec803deab90d14f28c2617f1e2ae2
Dengman <daen2000@student.miun.se>
2023-01-20 15:14:14 +0100

    Fixed so button changes text, added a lable

--- app/src/main/java/se/miun/dt170g/laboration01/MainActivity.java
+++ app/src/main/java/se/miun/dt170g/laboration01/MainActivity.java
@@ -2,13 +2,22 @@ package se.miun.dt170g.laboration01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = findViewById(R.id.textView);
+        Button button = findViewById(R.id.refreshButton);
+        button.setOnClickListener(v -> textView.setText("text1"));
+
     }
 }
