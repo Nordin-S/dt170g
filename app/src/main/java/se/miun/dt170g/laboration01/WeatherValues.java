025f92d6a94072f222db5764870af5f272b72121
anga95 <andreas.gabrielsson@live.se>
2023-01-20 15:35:26 +0100

    class that stores the WeatherValues, temperature, wind, cloudines andpercipitation

--- app/src/main/java/se/miun/dt170g/laboration01/WeatherValues.java
+++ app/src/main/java/se/miun/dt170g/laboration01/WeatherValues.java
@@ -1,6 +1,21 @@
 package se.miun.dt170g.laboration01;
 
 public class WeatherValues {
-    
+
+    float temperature_degree;
+    String temperature_unit = "Celsius";
+
+    float wind_speed;
+    String wind_unit = "mps";
+    String wind_direction;
+
+    float clodiness_percentage;
+
+    String percipitation_unit = "mm";
+    float percipitation_min;
+    float percipitation_max;
+
+
+
 
 }
