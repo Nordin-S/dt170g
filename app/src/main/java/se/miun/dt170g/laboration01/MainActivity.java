c010788d61e2410faa5404cba2618daa2dc5b28e
Dengman <daen2000@student.miun.se>
2023-01-20 16:41:30 +0100

    Testade och visa några bilder

--- app/src/main/java/se/miun/dt170g/laboration01/MainActivity.java
+++ app/src/main/java/se/miun/dt170g/laboration01/MainActivity.java
@@ -6,6 +6,7 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView img= (ImageView) findViewById(R.id.weatherPicture);
+        img.setImageResource(R.mipmap.ic_launcherstorm);
+
 
 
     }
