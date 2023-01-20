1a5c62e125f53746d8d2054becd04dd272833c55
fillesten <filipstenegren@hotmail.com>
2023-01-20 14:57:20 +0100

    Update WeatherValues.java

--- app/src/main/java/se/miun/dt170g/laboration01/WeatherValues.java
+++ app/src/main/java/se/miun/dt170g/laboration01/WeatherValues.java
@@ -1,6 +1,17 @@
 package se.miun.dt170g.laboration01;
 
 public class WeatherValues {
-    
+
+    float temperature;
+
+    void readXML(/* url */) {
+
+        //temperature = url.temperature;
+    }
+
+    /*
+    *
+    *
+    * */
 
 }
