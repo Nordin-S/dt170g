efe392db4c993d21b8e3fe7debf88fb695dce3ad
hejtlo0p <peter.stegeby@gmail.com>
2023-01-27 13:38:13 +0100

    Merged Lambda_function branch to weather_data_api branch.

--- app/src/main/java/se/miun/dt170g/laboration01/BackgroundProcess.java
+++ app/src/main/java/se/miun/dt170g/laboration01/BackgroundProcess.java
@@ -2,6 +2,6 @@ package se.miun.dt170g.laboration01;
 
 public class BackgroundProcess {
     void run(WeatherParser wp) {
-        wp.parseWeather(new DailyWeather());
+        wp.parseWeather(new DailyWeather(1,5.2, 2.1, "SW", 40, 0.0, 1.2));
     }
 }
